2a9aa37f686a291bfdafdd137d64c007a5074980
Blessing <bntoko@gmail.com>
2020-12-08 11:44:11 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,59 @@ export class AppComponent implements OnInit{
 
   public scene = new THREE.Scene();
   public renderer = new THREE.WebGLRenderer();
-  public camera = new THREE.PerspectiveCamera();
+  public camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
   public orbControls = new OrbitControls(this.camera, this.renderer.domElement);
+  public obj3DArr = [];
 
   ngOnInit() {
 
   }
 
   public init() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
 
+    this.camera.position.z = 5;
+    this.camera.lookAt(0, 0, 0);
+    this.orbControls.target.set(0, 0, 0);
+    this.orbControls.update();
+
+
+    window.addEventListener('resize', () => {
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+    });
+
+    const animate = (time) => {
+      time *= .001;
+
+      this.orbControls.update();
+
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+  }
+
+  public makeInstance(geometry, color, x, texture?) {
+    const material = new THREE.MeshPhongMaterial({
+      color,
+      map: texture ? texture : null
+    });
+
+    const obj3D = new THREE.Mesh(geometry, material);
+    this.scene.add(obj3D);
+
+    obj3D.position.x = x;
+    return obj3D;
+  }
+
+  public addLight(...pos) {
+    const color = 'white';
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(pos[0], pos[1], pos[2]);
+    this.scene.add(light);
   }
 }
