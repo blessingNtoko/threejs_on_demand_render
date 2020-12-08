59db3b87ec7dc6fa3b834f3cc86f1af0861b8c89
Blessing <bntoko@gmail.com>
2020-12-08 11:28:49 +0200

    setting up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'renderOnDemand';
+export class AppComponent implements OnInit{
+
+  public scene = new THREE.Scene();
+  public renderer = new THREE.WebGLRenderer();
+  public camera = new THREE.PerspectiveCamera();
+  public orbControls = new OrbitControls(this.camera, this.renderer.domElement);
+
+  ngOnInit() {
+
+  }
+
+  public init() {
+
+  }
 }
